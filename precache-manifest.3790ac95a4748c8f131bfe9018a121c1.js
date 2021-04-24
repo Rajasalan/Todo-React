f5cde95ae1a5d9789e7a812adff7cadee38c33f7
garland@1308 <garland.rajasalan@gmail.com>
2021-04-24 08:09:28 +0300

    Updates

---
+++ precache-manifest.3790ac95a4748c8f131bfe9018a121c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01a18c1da2e4516addb6f3a902d314b",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "2b256fce85e6777943b5",
+    "url": "/Todo-React/static/js/2.fb026b63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-React/static/js/2.fb026b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5800cc5f9425a1a0a015",
+    "url": "/Todo-React/static/js/main.26774bb1.chunk.js"
+  },
+  {
+    "revision": "6ceffd0cb5919f16034b",
+    "url": "/Todo-React/static/js/runtime-main.cf65c6d1.js"
+  },
+  {
+    "revision": "488079e1c14747c6b9627c17d17aea9c",
+    "url": "/Todo-React/static/media/login_img.488079e1.svg"
+  }
+]);
